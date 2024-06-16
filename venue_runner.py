46971e97ffa536ababfa849fcad1d56f0d6e06cd
The Edge Predictor <theedgepredictor@gmail.com>
2024-06-16 14:19:29 -0400

    Create venue_runner.py

---
+++ venue_runner.py
@@ -0,0 +1,18 @@
+# import espn-api-orm and the LeagueAPI and the VenueAPI
+
+# Get all valid sport league pairs 
+
+# For each LeagueAPI get the venue IDs
+
+# Try to get the saved sport league file if it exists.
+
+# Handle upsert logic for new or stale venue IDs
+
+# For each venue ID get the VenueAPI and get the venue attributes
+
+# Get the geocoding for the venue
+
+# Get the H3 index for the geocoding
+
+# Save the new Venues and Geocodings 
+
